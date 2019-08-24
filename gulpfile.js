5a3f7f3f5e78f728d59aaad932f0ae73fba52116
Andrzej <andrzej.michal.kolomanski@gmail.com>
2019-08-24 16:57:04 +0200

    TODO: fix the fucking sprite

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const {series, watch, src, dest} = require('gulp'),
+const {series, parallel, watch, src, dest} = require('gulp'),
 cssFiles = "app/styles/style.css",
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
@@ -6,7 +6,21 @@ cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
 browserSync = require('browser-sync').create(),
-mixins = require('postcss-mixins');
+mixins = require('postcss-mixins'),
+rename = require('gulp-rename'),
+svgSprite = require('gulp-svg-sprite'),
+config = {
+    mode:{
+        css:{
+            sprite: 'sprite.svg',
+            render:{
+                css:{
+                    template: './gulp/templates/sprite.css'
+                }
+            }
+        }
+    }
+};
 
 
 async function print(){
@@ -21,8 +35,8 @@ function html(cb){
 
 function styles(){
     return src(cssFiles)
-    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
-    .pipe(dest('app/temp/styles'));
+        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+        .pipe(dest('app/temp/styles'));
 }
 
 function cssInject(){
@@ -31,7 +45,23 @@ function cssInject(){
     
 }
 
+function createSprite (){
+    return src('./app/assets/images/icons/**/*.svg')
+        .pipe(svgSprite(config))
+        .pipe(dest('./app/temp/sprite'));
+    
+}
 
+function copySpriteGraphic (){
+    return src('./app/temp/sprite/css/**/*.svg')
+        .pipe(dest('./app/assets/images/sprites'));
+}
+
+function copySpriteCSS (){
+    return src('./app/temp/sprite/css/*.css')
+        .pipe(rename('_sprite.css'))
+        .pipe(dest('./app/styles/modules'));
+}
 
 
 
@@ -46,4 +76,6 @@ exports.default = function (){
     watch(['app/index.html'], html);
     watch(['app/styles/**/*.css'], series(styles, cssInject));
 };
-exports.haha = series(html);
+exports.createSprite = createSprite;
+exports.copySpriteCSS = copySpriteCSS;
+exports.icons = series(createSprite, parallel(copySpriteGraphic, copySpriteCSS));
